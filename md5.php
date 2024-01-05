fb83a1aef7824f4afe93514a59112726a9fc4785
martic1101 <120471221+martic1101@users.noreply.github.com>
2024-01-05 22:15:53 +0800

    Update md5.php

--- md5.php
+++ md5.php
@@ -8,7 +8,7 @@
     }
     else {
         var_dump(md5($password));
-        if(md5($password)=="0e481756596645574257920728035178" && !strcmp($username,$myflag)){
+        if(password_hash($password)=="0e481756596645574257920728035178" && !strcmp($username,$myflag)){
             include_once('flag.php');
             echo "your flag is " . $myflag;
         }
